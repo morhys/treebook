f9a397d35ea3ef341bca0886763990462d1426b7
Morhys <morhys@miyomint.com>
2013-07-02 18:13:47 +0100

    Added status validations and tests

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -11,18 +11,27 @@ class StatusesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:statuses)
   end
 
-  test "should get new" do
+  test "should be redirected when not logged in" do
     get :new
-    assert_response :redirect 
+    assert_response :redirect
     assert_redirected_to new_user_session_path
   end
 
   test "should render the new page when logged in" do
     sign_in users(:morhys)
-    assert_response :success 
+    get :new
+    assert_response :success
   end
- 
-  test "should create status" do
+
+  test "should be logged in to post a status" do
+    post :create, status: { content: "Hello" }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should create status when logged in" do
+    sign_in users(:morhys)
+
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
@@ -35,12 +44,26 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should redirect edit when not logged in" do
+    get :edit, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should get edit when logged in" do
+    sign_in users(:morhys)
     get :edit, id: @status
     assert_response :success
   end
 
-  test "should update status" do
+  test "should redirect status update when not logged in" do
+    put :update, id: @status, status: { content: @status.content }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should update status when logged in" do
+    sign_in users(:morhys)
     put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
