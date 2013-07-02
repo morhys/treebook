877f45d3ea160fe18685b93411610b2faa036a61
Morhys <morhys@miyomint.com>
2013-07-02 17:16:24 +0100

    Added tests for custom routes

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -13,12 +13,18 @@ class StatusesControllerTest < ActionController::TestCase
 
   test "should get new" do
     get :new
-    assert_response :success
+    assert_response :redirect 
+    assert_redirected_to new_user_session_path
   end
 
+  test "should render the new page when logged in" do
+    sign_in users(:morhys)
+    assert_response :success 
+  end
+ 
   test "should create status" do
     assert_difference('Status.count') do
-      post :create, status: { content: @status.content, name: @status.name }
+      post :create, status: { content: @status.content }
     end
 
     assert_redirected_to status_path(assigns(:status))
@@ -35,7 +41,7 @@ class StatusesControllerTest < ActionController::TestCase
   end
 
   test "should update status" do
-    put :update, id: @status, status: { content: @status.content, name: @status.name }
+    put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
 
