c3275e360e8386432b9b97ba2b5b1495644c5fc2
Morhys <morhys@miyomint.com>
2013-07-04 23:27:20 +0100

    added gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,13 @@ validates :profile_name, :presence => true,
   def full_name 
   	first_name + " " + last_name
   end
+
+  def gravatar_url
+    stripped_email = email.strip
+    downcased_email = stripped_email.downcase
+    hash = Digest::MD5.hexdigest(downcased_email)
+
+    "http://gravatar.com/avatar/#{hash}"
+  end
+
 end
